621d32e1df842e6f4b725ff2fdf09da1ab4c6005
Hélio Rocha <hjrocha@gmail.com>
2023-09-25 11:32:47 +0100

    move coverage to lcov format

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'simplecov', require: false
+gem 'simplecov-lcov'
 gem 'rubocop'
 gem 'brakeman'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 # spec/spec_helper.rb
 
 require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
